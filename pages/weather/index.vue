392eeda117e80fe34bf57858bd55d5626d827c1a
zcas12 <zcad8546@naver.com>
2023-03-21 22:13:13 +0900

    test

--- pages/weather/index.vue
+++ pages/weather/index.vue
@@ -46,7 +46,7 @@
         {{rehList}}
       </el-col>
     </el-row>-->
-    {{pcpList}}
+    {{rehList}}
   </div>
 </template>
 
@@ -105,14 +105,10 @@ export default {
     pcpList(){
       const pcp = _.cloneDeep(this.PCP);
       let hh = this.hour;
-      console.log("start")
-      console.log(pcp)
       const index = _.findIndex(this.PCP, { fcstDate: this.today, fcstTime: hh+"00" })
-      console.log(index)
+
       pcp.splice(0, index)
       pcp.length = 15;
-      console.log(pcp)
-      console.log("end")
       return pcp;
     },
     rehList(){
